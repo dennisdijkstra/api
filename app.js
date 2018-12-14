8454f74e6b17e6bf17ff54f74027b81b330d6bf9
Dennis <info@dennisdijkstra.nl>
2018-12-14 23:49:59 +0100

    create model and setup db

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
+const require('dotenv').config();
+const mongoose = require('mongoose');
 const express = require('express');
 const bodyParses = require('body-parser');
 const item = require('./routes/item.route');
 const app = express();
 
+const mongoDB = process.env.DB_URI;
+
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 app.use('/items', item);
 
 app.listen(3000, () => {
--- models/item.model.js
+++ models/item.model.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 let ItemSchema = new Schema({
-    id: {type: Number, required: true},
-    name: {type: String, required: true, max: 100},
+    id: { type: Number, required: true },
+    name: { type: String, required: true, max: 100 },
 });
 
 module.exports = mongoose.model('Item', ItemSchema);
