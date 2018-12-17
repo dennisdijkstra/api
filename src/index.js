55cec2228f8616ab8e89e6f641d81d257f27983a
Dennis <info@dennisdijkstra.nl>
2018-12-17 10:29:05 +0100

    install babel dependencies

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('Hello World');
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Component = () => {
+    return <div>Hello React!</div>;
+};
+
+ReactDOM.render(<Component />, document.getElementById("root"));
