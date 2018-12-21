8fbfe6113dca9ea5d0226dde2cc64925d6ad3e12
Dennis <info@dennisdijkstra.nl>
2018-12-21 21:20:49 +0100

    create user controller and add route

---
+++ controllers/user.controller.js
@@ -0,0 +1,25 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+exports.userCreate = (req, res) => {
+    const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+    });
+
+    bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (error, hash) => {
+            if (err) throw err;
+            newUser.password = hash;
+            newUser
+                .save()
+                .then(user => res.json(user))
+                .catch(errr => console.log(errr));
+        });
+    });
+
+    newUser.save()
+        .then(user => res.json(user));
+};
