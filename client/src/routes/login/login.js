94f9906147aacb7fc903850eb083e10f58315a82
Dennis <info@dennisdijkstra.nl>
2018-12-25 20:50:56 +0100

    create login form

--- client/src/routes/login/login.js
+++ client/src/routes/login/login.js
@@ -1,9 +1,34 @@
 import React, { Component } from 'react';
+import { Formik, Form, Field } from 'formik';
 
 class Register extends Component {
+    submit = (values, { setSubmitting, setStatus }) => {
+        if (values) {
+            console.log(values);
+            setSubmitting(false);
+            setStatus({ submitSucceeded: true });
+        }
+    };
+
     render() {
         return (
-            <h1>Login</h1>
+            <Formik
+                initialValues={{
+                    email: '',
+                    password: '',
+                }}
+                onSubmit={this.submit}
+            >
+                {({ dirty, isSubmitting }) => {
+                    return (
+                        <Form>
+                            <Field type="text" name="email" placeholder="Email" />
+                            <Field type="password" name="password" placeholder="Password" />
+                            <button disabled={!dirty || isSubmitting} type="submit">Login</button>
+                        </Form>
+                    );
+                }}
+            </Formik>
         );
     }
 }
