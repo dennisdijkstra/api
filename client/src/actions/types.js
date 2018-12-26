de2d02ff1782c5c338d8b4b158a57603f4686046
Dennis <info@dennisdijkstra.nl>
2018-12-26 23:41:49 +0100

    create user actions, add types

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -4,3 +4,7 @@ export const UPDATE_ITEM = 'UPDATE_ITEM';
 export const DELETE_ITEM = 'DELETE_ITEM';
 export const ITEMS_LOADING = 'ITEMS_LOADING';
 export const SET_IS_EDITABLE = 'SET_IS_EDITABLE';
+
+export const USER_LOADING = 'USER_LOADING';
+export const SET_CURRENT_USER = 'SET_CURRENT_USER';
+export const GET_CURRENT_USER = 'GET_CURRENT_USER';
---
+++ client/src/actions/userActions.js
@@ -0,0 +1,17 @@
+import {
+    USER_LOADING,
+    SET_CURRENT_USER,
+    GET_CURRENT_USER,
+} from './types';
+
+export const setUserLoading = () => (
+    
+);
+
+export const setCurrentUser = () => (
+
+);
+
+export const getCurrentUser = () => {
+
+};
