077515078ecc0032a2626c42a472ee0b98343c2f
Dennis <info@dennisdijkstra.nl>
2018-12-27 00:23:39 +0100

    set up user authentication actions

--- client/src/actions/userActions.js
+++ client/src/actions/userActions.js
@@ -1,11 +1,24 @@
-import jwt_decode from 'jwt-decode';
+import jwtDecode from 'jwt-decode';
 import {
     USER_LOADING,
     SET_CURRENT_USER,
     GET_CURRENT_USER,
 } from './types';
 
-export const registerUser = (user, history) => {
+export const setUserLoading = () => (
+    {
+        type: USER_LOADING,
+    }
+);
+
+export const setCurrentUser = decoded => (
+    {
+        type: SET_CURRENT_USER,
+        payload: decoded,
+    }
+);
+
+export const registerUser = (user, history) => () => {
     fetch('/api/users/register', {
         method: 'POST',
         headers: {
@@ -19,22 +32,36 @@ export const registerUser = (user, history) => {
         });
 };
 
-export const loginUser = () => {
-
+export const loginUser = user => (dispatch) => {
+    fetch('/api/users/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(user),
+    })
+        .then((res) => {
+            const { token } = res.data;
+            localStorage.setItem('jwtToken', token);
+            const decoded = jwtDecode(token);
+            dispatch(setCurrentUser(decoded));
+        })
+        .catch((err) => {
+            console.log(err);
+        });
 };
 
-export const setUserLoading = () => {
-
+export const getCurrentUser = () => (dispatch) => {
+    dispatch(setUserLoading());
+    fetch('/api/users/currentuser')
+        .then(response => response.json())
+        .then(res => dispatch({
+            type: GET_CURRENT_USER,
+            payload: res.data,
+        }));
 };
 
-export const setCurrentUser = () => {
-
-};
-
-export const getCurrentUser = () => {
-
-};
-
-export const logOutUser = () => {
-
+export const logOutUser = () => (dispatch) => {
+    localStorage.removeItem('jwtToken');
+    dispatch(setCurrentUser({}));
 };
