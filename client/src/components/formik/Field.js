2bf9dd7779c7e316fd9aae2dd757021cf1dd4f77
Dennis <info@dennisdijkstra.nl>
2018-12-27 19:24:16 +0100

    Install classnames. Form field error styling

--- client/src/components/formik/Field.js
+++ client/src/components/formik/Field.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Field } from 'formik';
+import classNames from 'classnames';
 import s from './Field.css';
 
 class InputField extends Component {
@@ -9,15 +10,22 @@ class InputField extends Component {
         return (
             <Field
                 name={name}
-                render={({ field, form: { touched, errors } }) => (
-                    <label htmlFor={name} className={s.label}>{label}
-                        <input {...field} type={name} id={name} placeholder={placeholder} />
-                        {touched[field.name]
-                            && errors[field.name]
-                            && <div className={s.error}>{errors[field.name]}</div>
-                        }
-                    </label>
-                )}
+                render={({ field, form: { touched, errors } }) => {
+                    const className = classNames({
+                        [`${s.label}`]: true,
+                        [`${s.error}`]: touched[field.name] && errors[field.name],
+                    });
+
+                    return (
+                        <label htmlFor={name} className={className}>{label}
+                            <input {...field} type={name} id={name} placeholder={placeholder} />
+                            {touched[field.name]
+                                && errors[field.name]
+                                && <div>{errors[field.name]}</div>
+                            }
+                        </label>
+                    );
+                }}
             />
         );
     }
--- client/src/validation/ValidationSchema.js
+++ client/src/validation/ValidationSchema.js
@@ -1,12 +1,18 @@
 import * as Yup from 'yup';
 
 const ValidationSchema = Yup.object().shape({
+    firstname: Yup
+        .string()
+        .required('First name is required'),
+    lastname: Yup
+        .string()
+        .required('Last name is required'),
     email: Yup
         .string()
         .min(3, 'Too short')
         .max(255, 'Too long')
-        .email('Invalid email address')
-        .required('Email is required'),
+        .email('Invalid e-mail address')
+        .required('E-mail is required'),
     password: Yup
         .string()
         .min(3, 'Too short')
