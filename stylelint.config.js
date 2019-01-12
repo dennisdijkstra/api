24f6c2df5e1bdb15b6441c9d9c81f27ee1fdd3c6
Dennis <info@dennisdijkstra.nl>
2019-01-12 21:02:06 +0100

    install stylelint

---
+++ stylelint.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    extends: 'stylelint-config-recess-order',
+    rules: {
+        indentation: 4,
+    },
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebPackPlugin = require('html-webpack-plugin');
+const StyleLintPlugin = require('stylelint-webpack-plugin');
 
 module.exports = {
     module: {
@@ -44,6 +45,9 @@ module.exports = {
         new HtmlWebPackPlugin({
             template: './client/src/index.html',
             filename: './index.html',
+        }),
+        new StyleLintPlugin({
+            files: ['src/**/*.css'],
         }),
     ],
 };
