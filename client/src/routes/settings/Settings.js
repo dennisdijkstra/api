d1afec8f29da1c72251d987b9765d6e359d22694
Dennis <info@dennisdijkstra.nl>
2019-01-14 19:15:44 +0100

    create user settings form

--- client/src/routes/settings/Settings.js
+++ client/src/routes/settings/Settings.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
+import { Formik, Form } from 'formik';
+import Field from '../../components/atoms/formik/Field';
+import { RegisterValidation } from '../../validation/ValidationSchema';
+
 
 class Settings extends Component {
     render() {
         return (
             <>
                 <h1>User settings</h1>
-                <button type="button">Save</button>
+                <Formik
+                    initialValues={{
+                        firstname: '',
+                        lastname: '',
+                        email: '',
+                    }}
+                    onSubmit={this.submit}
+                    validationSchema={RegisterValidation}
+                >
+                    {({ dirty, isSubmitting }) => (
+                        <>
+                            <Form>
+                                <Field name="firstname" label="First name" placeholder="First name" />
+                                <Field name="lastname" label="Last name" placeholder="Last name" />
+                                <Field name="email" label="E-mail" placeholder="E-mail" />
+                                <button disabled={!dirty || isSubmitting} type="submit">Save</button>
+                            </Form>
+                        </>
+                    )}
+                </Formik>
             </>
         );
     }
