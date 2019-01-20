5b02468ab3aeef00dd07c8ffe7cd57965fce966e
Dennis <info@dennisdijkstra.nl>
2019-01-20 20:28:41 +0100

    implement redux persist

--- client/src/store.js
+++ client/src/store.js
@@ -1,16 +1,23 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import rootReducer from './reducers';
 
-const initialState = {};
+const persistConfig = {
+    key: 'root',
+    storage,
+};
 
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+const initialState = {};
 const middleware = [thunk];
 
 /* eslint-disable no-underscore-dangle */
-const store = createStore(rootReducer, initialState, compose(
+export const store = createStore(persistedReducer, initialState, compose(
     applyMiddleware(...middleware),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 ));
-/* eslint-enable */
+export const persistor = persistStore(store);
 
-export default store;
+/* eslint-enable */
