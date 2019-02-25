7ab11caa5823c9f789cbc6dc816213f2b7518dfa
Dennis <info@dennisdijkstra.nl>
2019-02-25 21:35:33 +0100

    turn class components into functional components

--- client/src/components/atoms/formik/Field.js
+++ client/src/components/atoms/formik/Field.js
@@ -1,34 +1,28 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Field } from 'formik';
 import classNames from 'classnames';
 import s from './Field.css';
 
-class InputField extends Component {
-    render() {
-        const { name, label, placeholder } = this.props;
+const InputField = ({ name, label, placeholder }) => (
+    <Field
+        name={name}
+        render={({ field, form: { touched, errors } }) => {
+            const className = classNames({
+                [`${s.label}`]: true,
+                [`${s.error}`]: touched[field.name] && errors[field.name],
+            });
 
-        return (
-            <Field
-                name={name}
-                render={({ field, form: { touched, errors } }) => {
-                    const className = classNames({
-                        [`${s.label}`]: true,
-                        [`${s.error}`]: touched[field.name] && errors[field.name],
-                    });
-
-                    return (
-                        <label htmlFor={name} className={className}>{label}
-                            <input {...field} type={name} id={name} placeholder={placeholder} />
-                            {touched[field.name]
-                                && errors[field.name]
-                                && <div>{errors[field.name]}</div>
-                            }
-                        </label>
-                    );
-                }}
-            />
-        );
-    }
-}
+            return (
+                <label htmlFor={name} className={className}>{label}
+                    <input {...field} type={name} id={name} placeholder={placeholder} />
+                    {touched[field.name]
+                        && errors[field.name]
+                        && <div>{errors[field.name]}</div>
+                    }
+                </label>
+            );
+        }}
+    />
+);
 
 export default InputField;
